81e6cf6247cacc5e9fcca0ea0e001237e622db19
Anthony Coats <acoats93@gmail.com>
2020-02-11 09:41:48 -0600

    add journal reducer basic structure

--- src/redux/reducers/journalReducer.js
+++ src/redux/reducers/journalReducer.js
@@ -1,1 +1,57 @@
-//journal reducer
+import axios from 'axios';
+
+//initial state
+
+initialState = {
+    time: '',
+    content: ''
+}
+
+//constants
+
+const GET_JOURNAL = "GET_JOURNAL";
+const ADD_JOURNAL = "ADD_JOURNAL";
+const EDIT_JOURNAL = "EDIT_JOURNAL";
+const DELETE_JOURNAL = "DELETE_JOURNAL";
+
+//functions
+
+export const getJournal = () => {
+
+};
+
+export const addJournal = () => {
+
+};
+
+export const editJournal = () => {
+
+};
+
+export const deleteJournal = () => {
+
+};
+
+//reducers
+
+export const reducer = (state=initialState, action) => {
+    const {type, payload} = action;
+    switch(type){
+        case `${GET_JOURNAL}_FULFILLED`:
+            return{
+                ...state
+            }
+        case `${ADD_JOURNAL}_FULFILLED`:
+            return{
+                ...state
+            }
+        case `${EDIT_JOURNAL}_FULFILLED`:
+            return{
+                ...state
+            }
+        case `${DELETE_JOURNAL}_FULFILLED`:
+            return{
+                ...state
+            }
+    }
+}
