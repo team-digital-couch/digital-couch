b2b4ea9d8549fade1a656509dd750de2f55560cb
Anthony Coats <acoats93@gmail.com>
2020-02-11 10:01:44 -0600

    add provider notes basic structure

--- src/redux/reducers/journalReducer.js
+++ src/redux/reducers/journalReducer.js
@@ -53,5 +53,6 @@ export const reducer = (state=initialState, action) => {
             return{
                 ...state
             }
+        default: return state
     }
 }
--- src/redux/reducers/providerNotesReducer.js
+++ src/redux/reducers/providerNotesReducer.js
@@ -1,1 +1,51 @@
-//provider Notes
+//initial state
+
+//constants
+
+const GET_PROVIDER_NOTES = "GET_PROVIDER_NOTES";
+const ADD_PROVIDER_NOTES = "ADD_PROVIDER_NOTES";
+const EDIT_PROVIDER_NOTES = "EDIT_PROVIDER_NOTES";
+const DELETE_PROVIDER_NOTES = "DELETE_PROVIDER_NOTES";
+
+//functions
+
+export const getProviderNotes = () => {
+
+};
+
+export const addProviderNotes = () => {
+
+};
+
+export const editProviderNotes = () => {
+
+};
+
+export const deleteProviderNotes = () => {
+
+};
+
+//reducers
+
+export const reducer = (state=initialState, action) => {
+    const {type, payload} = action;
+    switch(type){
+        case `${GET_PROVIDER_NOTES}_FULFILLED`:
+            return{
+                ...state
+            }
+        case `${ADD_PROVIDER_NOTES}_FULFILLED`:
+            return{
+                ...state
+            }
+        case `${EDIT_PROVIDER_NOTES}_FULFILLED`:
+            return{
+                ...state
+            }
+        case `${DELETE_PROVIDER_NOTES}_FULFILLED`:
+            return{
+                ...state
+            }
+        default: return state
+    }
+}
