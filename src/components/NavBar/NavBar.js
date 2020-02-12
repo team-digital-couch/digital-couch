1a81022436f696a3905b491b65b66c773e7f5638
Phoenix Shane <phoenix.oconnell@gmail.com>
2020-02-12 11:45:49 -0600

    Add NavBar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class NavBar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default NavBar
